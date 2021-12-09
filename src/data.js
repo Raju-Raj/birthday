195a574369f7450f009c985f69a0270398289304
Bojja Raju <brajuraj1997@gmail.com>
2021-12-09 17:55:22 +0530

    birthday

---
+++ src/data.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line
+export default [
+    {
+        id:1,
+        name:'ramesh',
+        age:26,
+        image:'https://nettv4u.com/imagine/suresh.jpg'
+    },
+    {
+        id:2,
+        name:'suresh',
+        age:24,
+        image:'https://english.mathrubhumi.com/polopoly_fs/1.5514893.1615697801!/image/image.jpg_gen/derivatives/landscape_894_577/image.jpg'
+    },
+    {
+        id:3,
+        name:'dinesh',
+        age:29,
+        image:'https://keralakaumudi.com/web-news/en/2021/09/NMAN0275054/image/suresh-gopi.1.1275027.jpg'
+    },
+    {
+        id:4,
+        name:'mukesh',
+        age:28,
+        image:'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2016/07/16/482349-mukesh.jpg'
+    },
+    {
+        id:5,
+        name:'ganesh',
+        age:25,
+        image:'https://nettv4u.com/imagine/04-11-2019/ganesh-master.jpg'
+    }
+
+]
